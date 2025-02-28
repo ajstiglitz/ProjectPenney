02f77c8e71ffe20cd0a9682a29b68b0745cfdfd3
ajstiglitz <ajstiglitz@wm.edu>
2025-02-28 02:32:06 -0500

    as much as I can get done for now
    I dont have the code for the draws or the second heatmap but I have at least the one...

--- src/computing.py
+++ src/computing.py
@@ -1,19 +1,42 @@
 import numpy as np
 import pandas as pd
 import os
+from src.processing import ALL_PATTERNS
 
 CSV_PATH = os.path.join('data', 'scores.csv')
 
 # The sole purpose of this module is to read in the already computed scores
 # and turn them into an 8x8 array of probabilities
-
 def calc_probability(csv_file:str = CSV_PATH):
+    """
+    csv_file: it is the file which will be used to calculate probabilities.
+
+    The purpose of this function is to calculate the probability of winning.
+    Returns that probability in order for it to be used later in visualizations.
+    """
+
     df = pd.read_csv(CSV_PATH,dtype={'p1pattern': str, 'p2pattern': str})
     df = df.drop(columns = ['p1_pattern','p2_pattern'])
 
+    # creates 8x8 zero array to be filled in 
+    prob_array = np.zeros((8,8))
 
+    # Loop through all possible pattern combos
+    for i, p1 in enumerate(ALL_PATTERNS):
+        for j, p2 in enumerate(ALL_PATTERNS):
+            if p1 == p2:
+                continue # dont need to bother if p1 and p2 equal (because that's not possible for the game)
 
+            # Filtering rows
+            df_subset = df[(df['p1pattern'] == p1) & (df['p2pattern'] == p2)]
 
-    return np.ndarray
+            # Count Player 1 wins based on cards or tricks
+            p1_wins = ((df_subset['p1cards'] > df_subset['p2cards']) | 
+                       (df_subset['p1tricks'] > df_subset['p2tricks'])).sum()
+
+            # Compute probability
+            prob_array[i, j] = p1_wins / len(df_subset)
+
+    return prob_array
 
 
--- src/visualization.py
+++ src/visualization.py
@@ -1,24 +1,20 @@
 import src.processing
 import pandas as pd
 import seaborn as sns # type: ignore
+import matplotlib.pyplot as plt
+
+from src.computing import calc_probability
+ALL_PATTERNS = ['RRR', 'RBR', 'BRR', 'BBR', 'RRB', 'RBB', 'BRB', 'BBB']
 
 
 # create a heatmap that that uses the stored decks and scores rather than doing any kind of simulation itself
-#grey out patterns played against itself (like RRR and RRR for p1 and p2)
-#can put NaNs in
-#also can do % as whole number
-# like 49% winning and 3% draw
-#put in some indication of the sample size when the visualization pops up
-
-#show everything (other than the diagonal)
-
-def decks_heatmap(csv_path):
+def decks_heatmap():
     """
-    Creates a heatmap for visualizing the ideas on the Penney Game Wiki 
-    (should have similar probability results)
+    Creates a heatmap for visualizing the probabilities of winning based on the scored decks
     """
-    data = pd.read_csv(csv_path,index_col=0)
-    g = sns.heatmap(data)
-    
-    return
+    prob_matrix = calc_probability()
+    plt = sns.heatmap(prob_matrix, cmap="crest", 
+                      annot=True, linewidths = 0.5, 
+                      xticklabels=ALL_PATTERNS, yticklabels=ALL_PATTERNS,)
 
+    return plt
